adffaf0990ed7ec2b1a03d47e685483612ffcc5d
“neocrmoficial” <neocrmoficial@gmail.com>
2024-01-24 14:50:56 -0300

    doc: correção de bugs

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const text = document.querySelector(".container__text-area")
 const img = document.querySelector(".mensagem-area__img")
 const txt = document.querySelector(".mensagem-area__txt")
-const cody = [
+
+const matiz = [
     ['e', 'enter'],
     ['i', 'imes'],
     ['a', 'ai'],
@@ -10,47 +11,50 @@ const cody = [
 ];
 
 const cardsDiv = document.getElementById('cards');
+cardsDiv.addEventListener('input', function() {
+    const completeText = cardsDiv.textContent.trim();
+    botaoCopiar.style.display = completeText ? "block" : "none"; // Exibe o botão se houver conteúdo, senão oculta
+});
 
-function btnEncript() {
-    const textForEncript = encript(text.value);
+function btnEncrypt() {
+    encript(text.value);
     text.value = '';
-
 }
 
 function encript(stringCriptografy) {
     stringCriptografy = stringCriptografy.toLowerCase();
 
-    for(let i = 0; i < cody.length; i++) {
-        if(stringCriptografy.includes(cody[i][0])) {
-            stringCriptografy = stringCriptografy.replaceAll(cody[i][0], cody[i][1]);
+    for(let i = 0; i < matiz.length; i++) {
+        if(stringCriptografy.includes(matiz[i][0])) {
+            stringCriptografy = stringCriptografy.replaceAll(matiz[i][0], matiz[i][1]);
             img.style.display="none";
             txt.style.display="none";
         }
     }
-    criarCard(stringCriptografy);
-
+    createCard(stringCriptografy);
+    showBtnCopy();
     return stringCriptografy;
 }
 
-function bntDesencript() {
-    const stringCriptografada = text.value;
+function bntDecrypt() {
+    const stringEncrypted = text.value;
 
-    if (stringCriptografada) {
-        const resultadoDesencriptado = desencriptar(stringCriptografada, cody);
-        text.value = resultadoDesencriptado;
-        console.log(resultadoDesencriptado);
+    if (stringEncrypted) {
+        const resultDecrypted = decrypt(stringEncrypted, matiz);
+        text.value = resultDecrypted;
+        console.log(resultDecrypted);
     } else {
         console.error('A string criptografada está indefinida.');
     }
 }
 
 
-function desencriptar(stringCriptografada, cody) {
-    let desencriptada = stringCriptografada.toLowerCase();
+function decrypt(stringEncrypted, matiz) {
+    let decrypted = stringEncrypted.toLowerCase();
 
-    for (let i = 0; i < cody.length; i++) {
-        if (desencriptada.includes(cody[i][1])) {
-            desencriptada = desencriptada.replace(new RegExp(cody[i][1], 'g'), cody[i][0]);
+    for (let i = 0; i < matiz.length; i++) {
+        if (decrypted.includes(matiz[i][1])) {
+            decrypted = decrypted.replace(new RegExp(matiz[i][1], 'g'), matiz[i][0]);
 
         }
     }
@@ -61,46 +65,53 @@ function desencriptar(stringCriptografada, cody) {
         img.style.display = "none";
         txt.style.display = "none";
     }
-    criarCard(desencriptada);
 
-
-    return desencriptada;
+    createCard(decrypted);
+    showBtnCopy();
+    return decrypted;
 }
-function criarCard(texto) {
+function createCard(text) {
     const novoParagrafo = document.createElement('p');
-    novoParagrafo.textContent = texto;
+    novoParagrafo.textContent = text;
 
-    // Adiciona o resultado a um novo elemento <p>
     cardsDiv.appendChild(novoParagrafo);
     cardsDiv.appendChild(document.createElement('br'));
 }
 
 const botaoCopiar = document.getElementById('copy');
 botaoCopiar.addEventListener('click', function() {
-    copiarEremoverCards();
+    copyRemoveCards();
 });
 
-function copiarEremoverCards() {
+function copyRemoveCards() {
     const cardsDiv = document.getElementById('cards');
-    const textoCompleto = cardsDiv.textContent.trim(); // Obtém o texto dentro do campo cards
+    const completeText = cardsDiv.textContent.trim();
 
-    if (textoCompleto) {
-        copiarTextoParaClipboard(textoCompleto);
+    if (completeText) {
+        copyTextToClipboard(completeText);
         cardsDiv.innerHTML = '';
         img.style.display = "";
-        txt.style.display = ""; // Limpa o conteúdo do campo cards
+        txt.style.display = "";
+        hideButton()
 
     } 
 }
 
-function copiarTextoParaClipboard(texto) {
+function copyTextToClipboard(text) {
     const elementoTemporario = document.createElement('textarea');
-    elementoTemporario.value = texto;
+    elementoTemporario.value = text;
 
     document.body.appendChild(elementoTemporario);
     elementoTemporario.select();
     document.execCommand('copy');
     document.body.removeChild(elementoTemporario);
 
-    alert('Texto copiado para a área de transferência: ' + texto);
+    alert(`texto copiado para a área de transferência: ${text}`);
+}
+
+function showBtnCopy() {
+    botaoCopiar.style.display = "block";
+}
+function hideButton () {
+    botaoCopiar.style.display = "none";
 }
